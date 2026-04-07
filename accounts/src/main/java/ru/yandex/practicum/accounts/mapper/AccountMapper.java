ad14f39a2f31136aab1a8e013f8ab9d95c8eaed8
applegreen <igreenapple67@gmail.com>
2026-04-07 21:50:54 +0500

    Исправление замечаний - маппер для Account

---
+++ accounts/src/main/java/ru/yandex/practicum/accounts/mapper/AccountMapper.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.accounts.mapper;
+
+import ru.yandex.practicum.accounts.domain.Account;
+import ru.yandex.server.domain.UserAccount;
+import ru.yandex.server.domain.UserAccountSmall;
+
+public class AccountMapper {
+
+    public static UserAccount mapToUA(Account account) {
+        if (account == null) return null;
+
+        UserAccount userAccount = new UserAccount();
+        userAccount.setLogin(account.getLogin());
+        userAccount.setName(account.getName());
+        userAccount.setBirthdate(account.getBirthdate().toString());
+        userAccount.setSum(account.getAmount());
+        return userAccount;
+    }
+
+    public static UserAccountSmall mapToUAS(Account account) {
+        if (account == null) return null;
+
+        UserAccountSmall userAccount = new UserAccountSmall();
+        userAccount.setLogin(account.getLogin());
+        userAccount.setName(account.getName());
+        return userAccount;
+    }
+}
