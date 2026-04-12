3a72b4fc16d87a3f2526cfe14542d5bd1022921a
applegreen <igreenapple67@gmail.com>
2026-04-12 21:27:05 +0500

    Библиотека для модели сообщения

---
+++ notification-model/src/main/java/ru/yandex/practicum/notification/api/NotifyMessage.java
@@ -0,0 +1,49 @@
+package ru.yandex.practicum.notification.api;
+
+public class NotifyMessage {
+    private String id;
+    private String message;
+    private Boolean error;
+
+    public NotifyMessage() {
+    }
+
+    public NotifyMessage(String id, String message, Boolean error) {
+        this.id = id;
+        this.message = message;
+        this.error = error;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getError() {
+        return error;
+    }
+
+    public void setError(Boolean error) {
+        this.error = error;
+    }
+
+    @Override
+    public String toString() {
+        return "NotifyMessage{" +
+                "id='" + id + '\'' +
+                ", message='" + message + '\'' +
+                ", error=" + error +
+                '}';
+    }
+}
