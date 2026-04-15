beae12921cf68ad413cdaeb1818250df14637a7a
applegreen <igreenapple67@gmail.com>
2026-04-15 21:01:30 +0500

    Логгер вместо sout

--- accounts/src/main/java/ru/yandex/practicum/accounts/service/SumService.java
+++ accounts/src/main/java/ru/yandex/practicum/accounts/service/SumService.java
@@ -1,14 +1,19 @@
 package ru.yandex.practicum.accounts.service;
 
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.accounts.domain.Account;
 import ru.yandex.practicum.accounts.repository.AccountRepository;
+import ru.yandex.practicum.notification.client.NotificationProducer;
 
 import java.math.BigDecimal;
 
 @Component
 public class SumService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SumService.class);
+
     private final AccountRepository accountRepository;
 
     public SumService(AccountRepository accountRepository) {
@@ -20,7 +25,7 @@ public class SumService {
         Account account = accountRepository.findByLogin(login);
 
         if ("GET".equals(action) && account.getAmount().compareTo(value) < 0) {
-            System.out.println("Недостаточно средств");
+            LOGGER.error("У клиента {} недостаточно средств", login);
             return null;
         }
         BigDecimal newAmount = "GET".equals(action) ? account.getAmount().subtract(value) : account.getAmount().add(value) ;
--- accounts/src/main/java/ru/yandex/practicum/accounts/service/TransferService.java
+++ accounts/src/main/java/ru/yandex/practicum/accounts/service/TransferService.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.accounts.service;
 
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.accounts.domain.Account;
 import ru.yandex.practicum.accounts.repository.AccountRepository;
@@ -9,6 +11,7 @@ import java.math.BigDecimal;
 
 @Component
 public class TransferService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransferService.class);
 
     private final AccountRepository accountRepository;
 
@@ -21,7 +24,7 @@ public class TransferService {
         Account fromAccount = accountRepository.findByLogin(fromLogin);
 
         if (fromAccount.getAmount().compareTo(value) < 0) {
-            System.out.println("Недостаточно средств");
+            LOGGER.error("У клиента {} недостаточно средств", fromLogin);
             return null;
         }
 
