b1a76fdb98aea79483c2fea9b92b6d719f900048
applegreen <igreenapple67@gmail.com>
2026-04-25 22:08:00 +0500

    Отправка кастомных метрик transfer и cash

---
+++ cash/src/main/java/ru/yandex/practicum/cash/monitoring/Metric.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.cash.monitoring;
+
+public class Metric {
+    public static final String CASH_TOTAL = "cash_total";
+    public static final String CASH_SUCCESS = "cash_success";
+    public static final String CASH_FAIL = "cash_fail";
+}
--- cash/src/main/java/ru/yandex/practicum/cash/service/AccountsService.java
+++ cash/src/main/java/ru/yandex/practicum/cash/service/AccountsService.java
@@ -2,18 +2,35 @@ package ru.yandex.practicum.cash.service;
 
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.cash.client.AccountsClient;
+import ru.yandex.practicum.cash.monitoring.MonitoringClient;
 import ru.yandex.server.domain.CashRequest;
+
+import static ru.yandex.practicum.cash.monitoring.Metric.CASH_FAIL;
+import static ru.yandex.practicum.cash.monitoring.Metric.CASH_SUCCESS;
+import static ru.yandex.practicum.cash.monitoring.Metric.CASH_TOTAL;
 
 @Component
 public class AccountsService {
     private final AccountsClient client;
+    private final MonitoringClient monitoringClient;
 
-    public AccountsService(AccountsClient client) {
+    public AccountsService(AccountsClient client, MonitoringClient monitoringClient) {
         this.client = client;
+        this.monitoringClient = monitoringClient;
     }
 
     public Integer updateAccount(CashRequest request, String login) {
-        return client.updateSum(request, login);
+        monitoringClient.send(CASH_TOTAL, login);
+
+        Integer result;
+        try {
+            result = client.updateSum(request, login);
+            monitoringClient.send(CASH_SUCCESS, login);
+        } catch (Exception e) {
+            monitoringClient.send(CASH_FAIL, login);
+            throw e;
+        }
+        return result;
     }
 
 }
